589e0ec73e115083579b239e34b1690cb1749438
Fengjun Ma <FMa6@slb.com>
2022-10-27 11:28:18 +0800

    feature: can return message when speed up given car name

---
+++ OOPractice/Car.cs
@@ -0,0 +1,20 @@
+﻿namespace OOPractice
+{
+    using System;
+    public class Car
+    {
+        private string name;
+        private float speed;
+
+        public Car(string name, float speed)
+        {
+            this.name = name;
+            this.speed = speed;
+        }
+
+        public string SpeedUp()
+        {
+            return $"{name}: speed up {speed} km/h";
+        }
+    }
+}
